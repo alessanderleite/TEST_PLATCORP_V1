c85344bc286a2319e3bdec401d61dc49c5296e08
alessanderleite <alessander.logistica@gmail.com>
2019-08-10 12:31:16 -0300

    primeiro commit

---
+++ src/main/java/br/com/alessanderleite/TestPlatcorpV1Application.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPlatcorpV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPlatcorpV1Application.class, args);
+	}
+
+}
