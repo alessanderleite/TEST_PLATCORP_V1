e2247c9584e8e7c039fb1946aa16bf93c825995e
alessanderleite <alessander.logistica@gmail.com>
2019-08-11 19:14:39 -0300

    criando a interface ClienteTemperaturaService.java

---
+++ src/main/java/br/com/alessanderleite/service/ClienteTemperaturaService.java
@@ -0,0 +1,8 @@
+package br.com.alessanderleite.service;
+
+import br.com.alessanderleite.model.ClienteTemperatura;
+
+public interface ClienteTemperaturaService {
+
+	ClienteTemperaturaService criar(ClienteTemperatura clienteTemperatura);
+}
