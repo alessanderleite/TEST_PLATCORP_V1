c62a0e1772e9f92a9d565b91cd70d632b7a9082e
alessanderleite <alessander.logistica@gmail.com>
2019-08-12 15:45:20 -0300

    add ClienteTemperatura dentro da classe Cliente e @JoinColumns

--- src/main/java/br/com/alessanderleite/model/Cliente.java
+++ src/main/java/br/com/alessanderleite/model/Cliente.java
@@ -1,20 +1,28 @@
 package br.com.alessanderleite.model;
 
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "cliente")
-public class Cliente {
-	
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id")
-	private long id;
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
 	
 	@Column(name = "nome", nullable = false)
 	private String nome;
@@ -22,37 +30,53 @@ public class Cliente {
 	@Column(name = "idade", nullable = false)
 	private int idade;
 	
-	public Cliente() {
+	@OneToOne(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
+	@JoinColumns({@JoinColumn(name = "id_cliente_temperatura", referencedColumnName = "id", nullable = false)})
+	private ClienteTemperatura clienteTemperatura;
 	
+	public Cliente() {}
+	
+	public Cliente(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
 	}
-	
-	public Cliente(long id, String nome, int idade) {
+
+	public Cliente(Integer id, String nome, int idade) {
 		this.id = id;
 		this.nome = nome;
 		this.idade = idade;
 	}
-	
-	public long getId() {
+
+	public Integer getId() {
 		return id;
 	}
-	
-	public void setId(long id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public int getIdade() {
 		return idade;
 	}
-	
-	public void setAge(int idade) {
+
+	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+
+	public ClienteTemperatura getClienteTemperatura() {
+		return clienteTemperatura;
+	}
+
+	public void setClienteTemperatura(ClienteTemperatura clienteTemperatura) {
+		this.clienteTemperatura = clienteTemperatura;
 	}
 }
