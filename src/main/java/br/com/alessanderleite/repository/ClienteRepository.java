bb584f91c515a9e466b5a4a44e136bf307b8fe29
alessanderleite <alessander.logistica@gmail.com>
2019-08-12 18:40:40 -0300

    remove @Repository annotation

--- src/main/java/br/com/alessanderleite/repository/ClienteRepository.java
+++ src/main/java/br/com/alessanderleite/repository/ClienteRepository.java
@@ -3,11 +3,8 @@ package br.com.alessanderleite.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import br.com.alessanderleite.model.Cliente;
 
-@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	Optional<Cliente> findById(Integer id);
 }
