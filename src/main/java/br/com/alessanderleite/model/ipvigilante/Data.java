0e495be4cc20a160b2907f167d37f29e48b3109f
alessanderleite <alessander.logistica@gmail.com>
2019-08-12 16:52:01 -0300

    configura a classe Data como @Entity e cria relacionamentos

--- src/main/java/br/com/alessanderleite/model/ipvigilante/Data.java
+++ src/main/java/br/com/alessanderleite/model/ipvigilante/Data.java
@@ -1,26 +1,58 @@
 package br.com.alessanderleite.model.ipvigilante;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 /**
  * fonte de referência: https://ipvigilante.com/
  * 
  * @author Alessander
  *
  */
+@Entity
+@Table(name = "data")
 public class Data {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
 	private String ipv4;
 	private String continentName;
 	private String countryName;
 	private String subdivision1Name;
-	private Object subdivision2Name;
+	private String subdivision2Name;
 	private String cityName;
 	private String latitude;
 	private String longitude;
-
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "data")
+	private Set<Localizacao> localizacoes = new HashSet<Localizacao>(0);
+	
 	public Data() {}
 
-	public Data(String ipv4, String continentName, String countryName, String subdivision1Name, Object subdivision2Name,
-			String cityName, String latitude, String longitude) {
+	public Data(String ipv4, String continentName, String countryName, String cityName, String latitude,
+			String longitude) {
+		this.ipv4 = ipv4;
+		this.continentName = continentName;
+		this.countryName = countryName;
+		this.cityName = cityName;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public Data(Integer id, String ipv4, String continentName, String countryName, String subdivision1Name,
+			String subdivision2Name, String cityName, String latitude, String longitude,
+			Set<Localizacao> localizacoes) {
+		this.id = id;
 		this.ipv4 = ipv4;
 		this.continentName = continentName;
 		this.countryName = countryName;
@@ -29,6 +61,15 @@ public class Data {
 		this.cityName = cityName;
 		this.latitude = latitude;
 		this.longitude = longitude;
+		this.localizacoes = localizacoes;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getIpv4() {
@@ -63,11 +104,11 @@ public class Data {
 		this.subdivision1Name = subdivision1Name;
 	}
 
-	public Object getSubdivision2Name() {
+	public String getSubdivision2Name() {
 		return subdivision2Name;
 	}
 
-	public void setSubdivision2Name(Object subdivision2Name) {
+	public void setSubdivision2Name(String subdivision2Name) {
 		this.subdivision2Name = subdivision2Name;
 	}
 
@@ -95,10 +136,11 @@ public class Data {
 		this.longitude = longitude;
 	}
 
-	@Override
-	public String toString() {
-		return "Data [ipv4=" + ipv4 + ", continentName=" + continentName + ", countryName=" + countryName
-				+ ", subdivision1Name=" + subdivision1Name + ", subdivision2Name=" + subdivision2Name + ", cityName="
-				+ cityName + ", latitude=" + latitude + ", longitude=" + longitude + "]";
+	public Set<Localizacao> getLocalizacoes() {
+		return localizacoes;
+	}
+
+	public void setLocalizacoes(Set<Localizacao> localizacoes) {
+		this.localizacoes = localizacoes;
 	}
 }
