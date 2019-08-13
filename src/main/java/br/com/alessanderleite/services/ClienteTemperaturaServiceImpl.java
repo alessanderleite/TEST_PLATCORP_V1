a0ba60f0b2c27a45a32bd430bdae190e12213241
alessanderleite <alessander.logistica@gmail.com>
2019-08-12 23:55:40 -0300

    reimplementa ClienteTemperaturaService

--- src/main/java/br/com/alessanderleite/services/ClienteTemperaturaServiceImpl.java
+++ src/main/java/br/com/alessanderleite/services/ClienteTemperaturaServiceImpl.java
@@ -1,5 +1,8 @@
 package br.com.alessanderleite.services;
 
+import java.io.IOException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +12,43 @@ import br.com.alessanderleite.repository.ClienteTemperaturaRepository;
 @Service
 public class ClienteTemperaturaServiceImpl implements ClienteTemperaturaService{
 
-	@Autowired ClienteTemperaturaRepository clienteTemperaturaRepository;
+	@Autowired 
+	ClienteTemperaturaRepository clienteTemperaturaRepository;
 
 	@Override
-	public ClienteTemperatura criar(ClienteTemperatura clienteTemperatura) {
-		return clienteTemperaturaRepository.save(clienteTemperatura);
+	public Iterable<ClienteTemperatura> listAll() {
+		return clienteTemperaturaRepository.findAll();
 	}
-	
-	
+
+	@Override
+	public ClienteTemperatura getById(Integer id) throws IOException {
+		Optional<ClienteTemperatura> clienteTemperatura = clienteTemperaturaRepository.findById(id);
+		clienteTemperatura.orElseThrow(() -> new IOException("Não foi possível localizar o id " + id));
+		
+		return clienteTemperatura.get();
+	}
+
+	@Override
+	public ClienteTemperatura save(ClienteTemperatura entity) {
+		return clienteTemperaturaRepository.save(entity);
+	}
+
+	@Override
+	public void delete(Integer id) throws IOException {
+		Optional<ClienteTemperatura> clienteTemperatura = clienteTemperaturaRepository.findById(id);
+		if (!clienteTemperatura.isPresent()) {
+			throw new IOException("Não foi possível localizar o id " + id);
+		}
+		clienteTemperaturaRepository.deleteById(id);
+	}
+
+	@Override
+	public ClienteTemperatura update(ClienteTemperatura entity) throws IOException {
+		ClienteTemperatura clienteTemperatura = getById(entity.getId());
+		if (clienteTemperatura != null) {
+			return clienteTemperaturaRepository.save(entity);
+		}
+		return null;
+	}
+
 }
