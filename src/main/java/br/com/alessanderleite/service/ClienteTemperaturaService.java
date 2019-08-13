635fc7410a79d9dc1d1f68b138ad90d4d6846cde
alessanderleite <alessander.logistica@gmail.com>
2019-08-12 21:50:11 -0300

    deleted

--- src/main/java/br/com/alessanderleite/service/ClienteTemperaturaService.java
+++
@@ -1,8 +0,0 @@
-package br.com.alessanderleite.service;
-
-import br.com.alessanderleite.model.ClienteTemperatura;
-
-public interface ClienteTemperaturaService {
-
-	ClienteTemperatura criar(ClienteTemperatura clienteTemperatura);
-}
