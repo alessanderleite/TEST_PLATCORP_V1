cf61a7c1a0206c1daa18ea586e7a633f1f8ee2b4
alessanderleite <alessander.logistica@gmail.com>
2019-08-13 08:10:31 -0300

    adicona @EnableFeignClient para scanear interfaces com @FeignClient annotation

--- src/main/java/br/com/alessanderleite/config/FeignConfig.java
+++ src/main/java/br/com/alessanderleite/config/FeignConfig.java
@@ -1,5 +1,10 @@
 package br.com.alessanderleite.config;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(clients = {IpVigilanteClient.class, MetaweatherClient.class})
 public class FeignConfig {
 
 }
