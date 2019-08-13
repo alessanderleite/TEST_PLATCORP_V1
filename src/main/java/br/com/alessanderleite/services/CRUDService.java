f0b4d950ee0a5c1c41911e5ee4ae05770d8fb83f
alessanderleite <alessander.logistica@gmail.com>
2019-08-12 23:08:42 -0300

    faz os metodos extender Exception

--- src/main/java/br/com/alessanderleite/services/CRUDService.java
+++ src/main/java/br/com/alessanderleite/services/CRUDService.java
@@ -1,14 +1,16 @@
 package br.com.alessanderleite.services;
 
+import java.io.IOException;
+
 public interface CRUDService<T, ID> {
 
 	Iterable<T> listAll();
 	
-	T getById(ID id);
+	T getById(ID id) throws IOException;
 	
 	T save(T entity);
 	
-	void delete(ID id);
+	void delete(ID id) throws IOException;
 	
-	T update(T entity);
+	T update(T entity) throws IOException;
 }
