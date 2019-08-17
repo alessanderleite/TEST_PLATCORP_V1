28f8f4633d6b97e1962c95e2d991c83cb8155446
alessanderleite <alessander.logistica@gmail.com>
2019-08-16 23:02:00 -0300

    acrescenta os metodos alterar(), consultarPorId() e remover()

--- src/main/java/br/com/alessanderleite/controller/ClienteController.java
+++ src/main/java/br/com/alessanderleite/controller/ClienteController.java
@@ -1,12 +1,16 @@
 package br.com.alessanderleite.controller;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,23 +20,39 @@ import br.com.alessanderleite.model.Cliente;
 import br.com.alessanderleite.valueobject.Retorno;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/api/v1")
 public class ClienteController {
 
 	@Autowired
 	private ClienteBO clienteBO;
-	
-	@PostMapping
-	public ResponseEntity<?> salvar(@RequestBody Cliente cliente) throws URISyntaxException {
-		Retorno salvarCliente = clienteBO.salvar(cliente);
+
+	@PostMapping("/criar")
+	public ResponseEntity<?> criar(@RequestBody Cliente cliente) throws URISyntaxException {
+		Retorno clientSave = clienteBO.salvar(cliente);
+		String uri = "localhost:8080/clientes/"; 
 		return ResponseEntity
-				.created(new URI("localhost:8080/clientes/"+salvarCliente.getCliente().getId()))
-				.body(salvarCliente);
+				.created(new URI(uri+clientSave.getCliente().getId()))
+				.body(clientSave);
 	}
 	
-	@GetMapping
-	public ResponseEntity<?> listaTodosClientes() {
-		return ResponseEntity.ok().body(clienteBO.listaTodosClientes());
+	@PutMapping("/alterar")
+	public ResponseEntity<?> alterar(@RequestBody Retorno cliente) throws IOException {
+		return ResponseEntity.ok().body(clienteBO.atualizar(cliente));
+	}
+
+	@GetMapping("/consultar/{id}")
+	public ResponseEntity<?> consultarPorId(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(clienteBO.buscarClienteId(id));
 	}
 	
+	@GetMapping("/consultar")
+	public ResponseEntity<?> listarTodos() {
+		return ResponseEntity.ok().body(clienteBO.listaClientes());
+	}
+	
+	@DeleteMapping("/remover/{id}")
+	public ResponseEntity<?> remover(@PathVariable Integer id) {
+		clienteBO.deletar(id);
+		return ResponseEntity.ok().build();
+	}
 }
